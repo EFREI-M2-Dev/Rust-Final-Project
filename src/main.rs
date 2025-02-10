4bba6dafdfa05d6cb6f4728675ea51ac3b916a25
Suylo <spoownz@yahoo.com>
2025-02-10 17:22:15 +0100

    feat(interface): base interface menu selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+mod menu;
+mod counter;
+
+use crossterm::event::{self, Event, KeyEventKind};
+use ratatui::{DefaultTerminal, Frame};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::new().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}
+
+#[derive(Debug)]
+pub struct App {
+    state: AppState,
+}
+
+#[derive(Debug)]
+enum AppState {
+    Menu(menu::Menu),
+    Counter(counter::Counter),
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self {
+            state: AppState::Menu(menu::Menu::new()),
+        }
+    }
+
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.should_exit() {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn should_exit(&self) -> bool {
+        matches!(self.state, AppState::Counter(counter::Counter { exit: true, .. })) ||
+            matches!(self.state, AppState::Menu(menu::Menu { exit: true, .. }))
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        match &self.state {
+            AppState::Menu(menu) => menu.draw(frame),
+            AppState::Counter(counter) => counter.draw(frame),
+        }
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match &mut self.state {
+                    AppState::Menu(menu) => {
+                        if let Some(selection) = menu.handle_key_event(key_event) {
+                            self.state = AppState::Counter(counter::Counter::new(selection));
+                        }
+                    }
+                    AppState::Counter(counter) => counter.handle_key_event(key_event),
+                }
+            }
+            _ => {}
+        };
+        Ok(())
+    }
 }
---
+++ src/menu.rs
@@ -0,0 +1,92 @@
+use crossterm::event::{KeyCode, KeyEvent};
+use ratatui::{
+    layout::{Constraint, Direction, Layout},
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph},
+};
+
+#[derive(Debug)]
+pub struct Menu {
+    pub exit: bool,
+    options: Vec<&'static str>,
+    selected_index: usize,
+}
+
+impl Menu {
+    pub fn new() -> Self {
+        Self {
+            exit: false,
+            options: vec!["Lune", "Astéroïde", "Planète"],
+            selected_index: 0,
+        }
+    }
+
+    pub fn draw(&self, frame: &mut ratatui::Frame) {
+        let area = frame.area();
+
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+                Constraint::Percentage(40),
+                Constraint::Percentage(20),
+            ])
+            .split(area);
+
+        let border_block = Block::bordered().border_set(border::THICK);
+        frame.render_widget(border_block, area);
+
+        let title = Line::from(" Bienvenue sur EREEA ").bold();
+        let instructions = Line::from(" Esc pour quitter ".red());
+
+        let block = Block::bordered().title(title.centered()).title_bottom(instructions.centered());
+        frame.render_widget(block, frame.area());
+
+        let description = Paragraph::new(Text::from(vec![
+            Line::from("Salut ! Commence par choisir un type de planète :)"),
+            Line::from("Utilise les flèches 'haut' et 'bas' si tu veux naviguer entre les différentes options"),
+        ]))
+            .centered();
+
+        frame.render_widget(description, layout[1]);
+
+        let options: Vec<Line> = self.options.iter()
+            .enumerate()
+            .map(|(i, &option)| {
+                if i == self.selected_index {
+                    Line::from(format!("> {}", option).yellow().bold())
+                } else {
+                    Line::from(format!("  {}", option))
+                }
+            })
+            .collect();
+
+        let menu = Paragraph::new(options).centered();
+
+        frame.render_widget(menu, layout[2]);
+    }
+
+    pub fn handle_key_event(&mut self, key_event: KeyEvent) -> Option<&'static str> {
+        match key_event.code {
+            KeyCode::Esc => {
+                self.exit = true
+            }
+            KeyCode::Up => {
+                if self.selected_index > 0 {
+                    self.selected_index -= 1;
+                }
+            }
+            KeyCode::Down => {
+                if self.selected_index < self.options.len() - 1 {
+                    self.selected_index += 1;
+                }
+            }
+            KeyCode::Enter => return Some(self.options[self.selected_index]),
+            _ => {}
+        }
+        None
+    }
+}
